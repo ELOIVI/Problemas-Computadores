ed3a75d8838d34751ba6bce67794925168f66661
Eloi Viciana Gómez <69468823+ELOIVI@users.noreply.github.com>
2026-01-27 16:35:22 +0100

    Fix: Corregir cálculo de fracción y error de inicialización en metrónomo
    Cambios realizados:
    1. Corregir fórmula de cálculo de 'fraccion'
    2. Mover inicialización de 'fraccion' de global a main()

--- Soluciones/35/main.c
+++ Soluciones/35/main.c
@@ -15,20 +15,37 @@ unsigned char accent = 4;                               // Divisiones para gener
 unsigned char paused = !0;                              // Metrónomo pausado? (!0: sí, 0: no).
 
 unsigned int ang_actual = 0;                            // Ángulo actual (en formato Q12).
-unsigned int fraccion = ((bpm / accent) / 10) << 12;    // Incremento del ángulo en un VBL (Q12).
+unsigned int fraccion;								    // Incremento del ángulo en un VBL (Q12).
+														// No se puede inicializar aquí porque las 
+                                                        // variables globales en C requieren inicializadores
+                                                        // constantes, y la expresión depende de otras variables.
+														// Por lo tanto, o hardcodeamos los valores, o bien la
+														// inicializamos en el main.
 
 int main(void)
 {
     inicializaciones();
+
+	// La fórmula que surge al plantear el problema es la siguiente: ((bpm/accent)/10) << 12. 
+	// Esta funciona perfectamente cuando el BPM es superior o igual a 40. Pero el problema
+	// es que al hacer (bpm/accent), estamos haciendo una división entera, lo mismo pasa al
+	// dividir entre 10. Entonces al ejecutar el left shift, ya hemos perdido muchos decimales.
+	// La solución es simple, debemos hacer primero la multiplicación por 4096 y luego las divisiones.
+	fraccion = (bpm << 12) / (accent * 10);
+
     do
     {
         tareas_independientes();
         if (gestionar_botones())
         {                                               // Actualizar fracción según nuevos
-	    fraccion = ((bpm / accent) / 10) << 12;     // valores de bpm i accent.
+	    	fraccion = (bpm << 12) / (accent * 10); 	// valores de bpm i accent.
         }
         swiWaitForVBlank();
         actualizar_pantallas();
-        // activar_beat();
+		
+        // activar_beat();								// En el archivo "activar_beat.md" se justifica el porqué 
+														// llamamos a la función desde la RSI. Podríamos llamarla
+														// desde aquí perfectamente.
     } while(1);
+	return(0);
 }
